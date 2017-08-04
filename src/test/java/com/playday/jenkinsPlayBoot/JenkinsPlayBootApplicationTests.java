9310bef2d85f4066fbf3badfed7189951b91da93
Richard Watts-Seale <rwattsseale@gmail.com>
2017-08-04 11:56:48 +1000

    Add test

--- src/test/java/com/playday/jenkinsPlayBoot/JenkinsPlayBootApplicationTests.java
+++ src/test/java/com/playday/jenkinsPlayBoot/JenkinsPlayBootApplicationTests.java
@@ -1,5 +1,6 @@
 package com.playday.jenkinsPlayBoot;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,11 @@ public class JenkinsPlayBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void failingTest() {
+		Assert.assertTrue(false);
 	}
 
 }
